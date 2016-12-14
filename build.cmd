4e4fee19885b635f5e8f8e5c880678367efc8223
stefan.werner <stefan.werner@evopro-ag.de>
2016-12-14 15:53:09 +0100

    - adapted build script and solution

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 
-MSBuild.exe GnatMQ.sln /p:Configuration=Release
+REM "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" GnatMQ.sln /p:Configuration=Release
 
 IF NOT EXIST ".\Build\Packages" MKDIR ".\Build\Packages"
 
